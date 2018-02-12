06d85c380f5cde631d773e0656e4398cf2148120
Hoàng Tâm <phanhoangtam.pht@gmail.com>
2018-02-12 17:10:30 +0700

    Create notification channel for API 26+

---
+++ app/src/main/java/pht/eatitserver/helper/Notification.java
@@ -0,0 +1,61 @@
+package pht.eatitserver.helper;
+
+import android.annotation.TargetApi;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.ContextWrapper;
+import android.net.Uri;
+import android.os.Build;
+import pht.eatitserver.R;
+
+public class Notification extends ContextWrapper {
+
+    private static final String CHANNEL_ID = "pht.eatitserver.PHT";
+    private static final String CHANNEL_NAME = "Eat It";
+
+    private NotificationManager manager;
+
+    public Notification(Context base) {
+        super(base);
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            createChannel();
+        }
+    }
+
+    @TargetApi(Build.VERSION_CODES.O)
+    private void createChannel() {
+        NotificationChannel channel = new NotificationChannel(
+                CHANNEL_ID,
+                CHANNEL_NAME,
+                NotificationManager.IMPORTANCE_DEFAULT
+        );
+
+        channel.enableLights(false);
+        channel.enableVibration(true);
+        channel.setLockscreenVisibility(android.app.Notification.VISIBILITY_PRIVATE);
+
+        getManager().createNotificationChannel(channel);
+    }
+
+    public NotificationManager getManager() {
+        if(manager == null){
+            manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        }
+
+        return manager;
+    }
+
+    @TargetApi(Build.VERSION_CODES.O)
+    public android.app.Notification.Builder getNotification(String title, String body, PendingIntent pendingIntent, Uri sound){
+        return new android.app.Notification.Builder(getApplicationContext(), CHANNEL_ID)
+                .setSmallIcon(R.mipmap.ic_launcher_round)
+                .setContentTitle(title)
+                .setContentText(body)
+                .setSound(sound)
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(false);
+    }
+}
--- app/src/main/java/pht/eatitserver/service/FirebaseMessaging.java
+++ app/src/main/java/pht/eatitserver/service/FirebaseMessaging.java
@@ -6,17 +6,50 @@ import android.content.Context;
 import android.content.Intent;
 import android.media.RingtoneManager;
 import android.net.Uri;
+import android.os.Build;
 import android.support.v4.app.NotificationCompat;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
+import java.util.Random;
+import pht.eatitserver.OrderList;
 import pht.eatitserver.R;
 import pht.eatitserver.Welcome;
+import pht.eatitserver.global.Global;
+import pht.eatitserver.helper.Notification;
 
 public class FirebaseMessaging extends FirebaseMessagingService {
 
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
-        sendNotification(remoteMessage);
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            sendNotification26(remoteMessage);
+        } else {
+            sendNotification(remoteMessage);
+        }
+    }
+
+    private void sendNotification26(RemoteMessage remoteMessage) {
+        RemoteMessage.Notification notification = remoteMessage.getNotification();
+        String title = notification.getTitle();
+        String body = notification.getBody();
+
+        Intent orderList = new Intent(this, OrderList.class);
+        orderList.putExtra("phone", Global.activeUser.getPhone());
+        orderList.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, orderList, PendingIntent.FLAG_ONE_SHOT);
+        Uri defaultSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+
+        Notification helper = new Notification(this);
+
+        android.app.Notification.Builder builder = helper.getNotification(
+                title,
+                body,
+                pendingIntent,
+                defaultSound
+        );
+
+        // Random to show all notifications
+        helper.getManager().notify(new Random().nextInt(), builder.build());
     }
 
     private void sendNotification(RemoteMessage remoteMessage) {
